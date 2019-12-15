c46e2fada681d708a9596cadd2c7f10dbe3df590
onTuesday <mszombie1999@gmail.com>
2019-12-15 22:34:06 +0300

    Добавлен класс результата

---
+++ Source/FindFile/Result.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Collections;
+
+namespace FindFile
+{
+    public class Result
+    {
+        public static List<string> result = new List<string>();
+    }
+}
